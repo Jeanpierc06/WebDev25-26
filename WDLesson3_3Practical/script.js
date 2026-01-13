3da8f4a1e5347b2b1e847ff186d99ac1323ce23b
Jeanpierc06 <JEanpierc@nycstudents.net>
2026-01-13 18:10:03 +0000

    gyjuku

--- WDLesson3_3Practical/script.js
+++ WDLesson3_3Practical/script.js
@@ -1,1 +1,15 @@
 /* Challenge 2: Create a function for each image that display a description of the picture into the div with an id of highlight*/
+function www3school()
+document getElementById("highlight") .InnerHTML="www3school"
+
+function www3school2()
+document.getElementById("highlight"); InnerHTML.www3school2
+
+function www3school3()
+document.getElementById("highlight"); InnerHTML.www3school3
+
+function www3school4()
+document.getElementById("highlight"); InnerHTML.www3school4
+
+function www3school5()
+document.getElementById("highlight"); InnerHTML.www3school5
